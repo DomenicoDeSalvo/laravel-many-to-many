e68bb1252d656e7d0201798d0c6886d27b040f00
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-06-10 16:48:56 +0200

    Optimizations

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -18,7 +18,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with(['type'])->get();
         
         return view('admin.projects.index', compact('projects'));
     }
